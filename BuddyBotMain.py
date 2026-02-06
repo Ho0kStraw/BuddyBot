b56b78c0cb59afdae6336a4c6b0ca404d6672b48
Ho0kStraw <fawwaz.parwani.k@gmail.com>
2026-02-06 16:33:04 +0100

    BuddybotMain 0.1
    Eerste skelet voor Buddybot.

---
+++ BuddyBotMain.py
@@ -0,0 +1,25 @@
+#Hier importeren we de benodigde libraries voor de Discord Bot integration.
+
+
+#Alvast de discord token hier gezet, want die gaan we nodig hebben.
+DISCORD_TOKEN=MTQ2OTA1MTk4MjgwNjUxNTcyMg.GjgoCA.KW5aw_FTFpw5ql0GH5JNoxQMwr_TPQuaKWRZoY
+
+#startup banner en welkom message.(alleen cosmetisch, later correct implementeren met de bot.
+print(""" 
+ _____                                                                      
+|  ___|_ ___      ____      ____ _ ____                                     
+| |_ / _` \ \ /\ / /\ \ /\ / / _` |_  /                                     
+|  _| (_| |\ V  V /  \ V  V / (_| |/ /                                      
+|_|__\__,_| \_/\_/    \_/\_/_\__,_/___|              _   _     _       _    
+ ( _ )                   / ___|_ __ ___  _   _ _ __ | |_| |__ (_)_ __ | | __
+ / _ \/\                | |  _| '__/ _ \| | | | '_ \| __| '_ \| | '_ \| |/ /
+| (_>  <                | |_| | | | (_) | |_| | |_) | |_| | | | | | | |   < 
+ \___/\/          _      \____|_|  \___/ \__,_| .__/ \__|_| |_|_|_| |_|_|\_
+|  \/  |_   _ ___| |_ __ _ / _| __ _          |_|                           
+| |\/| | | | / __| __/ _` | |_ / _` |                                       
+| |  | | |_| \__ \ || (_| |  _| (_| |                                       
+|_|  |_|\__,_|___/\__\__,_|_|  \__,_|                                       
+
+""")
+print()
+print("Buddybot is wakker geworden, wat wil je doen?,\n/help voor commando's")
